6dae61861aa02f1be971256f9828a0f8cbc6a6b5
Michael Raguso <michael.raguso@gmail.com>
2018-02-11 16:26:24 -0500

    slowly start converting to es6 and start building api

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Music List' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Music List Alpha' });
+});
+
+const testJSON = [
+  {
+    name: 'John Smith',
+    username: 'CaptainCode'
+  },
+  {
+    name: 'Jane Doe',
+    username: 'JaneyCakes'
+  }
+];
+
+router.get('/sendjson', (req, res, next) => {
+  res.json(testJSON); // to send JSON
 });
 
 module.exports = router;
