4e255e5a0d25669e4affde9a667017449dd1969f
Michael Raguso <michael.raguso@gmail.com>
2018-02-11 16:38:56 -0500

    moved to develop branch

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,5 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({ test: 'MusicList API Test' });
-});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,5 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Music List Alpha' });
 });
 
-const testJSON = [
-  {
-    name: 'John Smith',
-    username: 'CaptainCode'
-  },
-  {
-    name: 'Jane Doe',
-    username: 'JaneyCakes'
-  }
-];
-
-router.get('/sendjson', (req, res, next) => {
-  res.json(testJSON); // to send JSON
-});
 
 module.exports = router;
