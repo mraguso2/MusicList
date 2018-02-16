d44c724268f4406870973098aae8d330735ee2f8
Michael Raguso <michael.raguso@gmail.com>
2018-02-16 17:14:53 -0500

    added sass and mock up login page

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,20 @@ module.exports = {
           use: 'css-loader',
         }),
       },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+          fallback: 'style-loader',
+        }),
+      },
     ],
   },
   plugins: [
