e5175eeb4da9ae3c1e744a36e2ee195063155bc3
Michael Raguso <michael.raguso@gmail.com>
2018-03-04 22:31:32 -0500

    add error handling

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'; // combineReducers mash all reducers into one big obj
 import AuthenticationReducer from '../reducers/authentication';
+import ErrorReducer from '../reducers/error';
 import ProgressReducer from '../reducers/progress';
 
 const reducers = {
   authentication: AuthenticationReducer,
+  error: ErrorReducer,
   progress: ProgressReducer,
 };
 
