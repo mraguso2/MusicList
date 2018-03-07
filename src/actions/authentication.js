b64fe0c35348f9287a209b8398cb8a2a39995238
Michael Raguso <michael.raguso@gmail.com>
2018-03-06 20:18:38 -0500

    added more content to password reset

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -11,8 +11,9 @@ export const logoutSuccess = () => ({ type: 'AUTHENTICATION_LOGOUT_SUCCESS' });
 export const registrationFailure = error => ({ type: 'AUTHENTICATION_REGISTRATION_FAILURE', error });
 export const registrationSuccess = () => ({ type: 'AUTHENTICATION_REGISTRATION_SUCCESS' });
 export const registrationSuccessViewed = () => ({ type: 'AUTHENTICATION_REGISTRATION_SUCCESS_VIEWED' });
+export const passwordResetClear = () => ({ type: 'AUTHENTICATION_PASSWORD_RESET_CLEAR' });
 export const passwordResetHashCreated = () => ({ type: 'AUTHENTICATION_PASSWORD_RESET_HASH_CREATED' });
-export const passwordResetHashFailure = () => ({ type: 'AUTHENTICATION_PASSWORD_RESET_HASH_FAILURE' });
+export const passwordResetHashFailure = error => ({ type: 'AUTHENTICATION_PASSWORD_RESET_HASH_FAILURE', error });
 export const sessionCheckFailure = () => ({ type: 'AUTHENTICATION_SESSION_CHECK_FAILURE' });
 export const sessionCheckSuccess = json => ({ type: 'AUTHENTICATION_SESSION_CHECK_SUCCESS', json });
 
@@ -48,6 +49,12 @@ export function checkSession() {
 // Send email to API for hashing
 export function createHash(email) {
   return async (dispatch) => {
+    // clear the error box if it's displayed
+    dispatch(clearError());
+
+    // turn on spinner
+    dispatch(incrementProgress());
+
     // contact the API
     await fetch(
       // where to contact
@@ -69,12 +76,15 @@ export function createHash(email) {
         return null;
       })
       .then((json) => {
-        if (json.username) {
+        if (json.success) {
           return dispatch(passwordResetHashCreated(json));
         }
         return dispatch(passwordResetHashFailure(new Error('Something went wrong. Please try again.')));
       })
       .catch(error => dispatch(passwordResetHashFailure(error)));
+
+    // turn off spinner
+    return dispatch(decrementProgress());
   };
 }
 
