c81ebcc5564c0c845090448638573d6f71195f0b
Michael Raguso <michael.raguso@gmail.com>
2018-03-08 20:53:58 -0500

    hook up external discogs api, build out albums page and search functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'; // combineReducers mash all reducers into one big obj
+import AlbumsReducer from '../reducers/albums';
 import AuthenticationReducer from '../reducers/authentication';
 import ErrorReducer from '../reducers/error';
 import ProgressReducer from '../reducers/progress';
 
 const reducers = {
+  albums: AlbumsReducer,
   authentication: AuthenticationReducer,
   error: ErrorReducer,
   progress: ProgressReducer,
